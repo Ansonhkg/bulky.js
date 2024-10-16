51e6d201013ef11430a2802303fa8742ec81096c
Anson <ansonox@gmail.com>
2024-10-16 02:10:09 +0100

    feat: add exports

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { Bulkie } from "./bulkie";
+export { BulkieUtils } from "./utils";
+export {
+  FN,
+  STEP,
+  UNAVAILABLE_STEP,
+  BulkieSupportedFunctions,
+  FunctionReturnTypes,
+  HexAddress,
+  IPFSCIDv0,
+  AuthMethodScopes
+} from "./types";
