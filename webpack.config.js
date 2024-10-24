bae84645ff7461bb859b6ecb8637fb5e7f328f64
Anson <ansonox@gmail.com>
2024-10-24 04:37:01 +0100

    feat: umd bundle works

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: ["buffer", "./src/index.ts"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bulkie.min.js",
+    library: "Bulkie", // Changed this
+    libraryTarget: "umd", // Changed to use libraryTarget instead of library.type
+    libraryExport: "Bulkie", // Added this
+    globalObject: "this",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer/"),
+      util: require.resolve("util/"),
+      process: require.resolve("process/browser"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              compilerOptions: {
+                noEmit: false,
+                module: "esnext",
+                target: "es2020",
+              },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "production"
+      ),
+      "process.env.DEBUG": JSON.stringify(process.env.DEBUG || false),
+    }),
+  ],
+};
